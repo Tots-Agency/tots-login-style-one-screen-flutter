29df10aa4fa14978b555fef253d20d3c33efbd4c
Matias Camiletti <matias.camiletti@gmail.com>
2023-05-14 10:46:40 -0300

    Add dynamic image, title and subtitle.

--- lib/src/screens/login_style_one.screen.dart
+++ lib/src/screens/login_style_one.screen.dart
@@ -4,8 +4,11 @@ import 'package:tots_theme/tots_theme.dart';
 class LoginStyleOneScreen extends StatefulWidget {
 
   final Function(String, String)? onLogin;
+  final String? imageLogo;
+  final String? title;
+  final String? subtitle;
 
-  const LoginStyleOneScreen({Key? key, this.onLogin}) : super(key: key);
+  const LoginStyleOneScreen({Key? key, this.onLogin, this.imageLogo, this.title, this.subtitle}) : super(key: key);
 
   @override
   _LoginStyleOneScreenState createState() => _LoginStyleOneScreenState();
@@ -33,7 +36,7 @@ class _LoginStyleOneScreenState extends State<LoginStyleOneScreen> {
 
   // State field(s) for PasswordField widget.
   TextEditingController? passwordFieldController;
-  bool passwordFieldVisibility = true;
+  bool passwordFieldVisibility = false;
   String? passwordFieldControllerValidator(String? val) {
     if (val == null || val.isEmpty) {
       return 'Password is required.';
@@ -61,6 +64,137 @@ class _LoginStyleOneScreenState extends State<LoginStyleOneScreen> {
     super.dispose();
   }
 
+  Widget _buildEmailField() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            SelectionArea(
+                child: Text(
+              'Email',
+              style: TotsTheme().bodyMedium().copyWith(fontWeight: FontWeight.w500),
+            )),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 6, 0, 0),
+          child: TextFormField(
+            controller: emailAddressFieldController,
+            obscureText: false,
+            decoration: InputDecoration(
+              labelStyle: TotsTheme()
+                  .bodyMedium()
+                  .copyWith(
+                    color: TotsTheme().primaryText,
+                    fontSize: 16,
+                  ),
+              hintText: 'Enter your email',
+              hintStyle: TotsTheme()
+                  .bodyMedium()
+                  .copyWith(
+                    color: TotsTheme().secondaryText,
+                    fontSize: 16,
+                  ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Color(0xFFD0D5DD),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Color(0x00000000),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              errorBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Color(0xFFFDA29B),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Color(0xFFFDA29B),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              contentPadding: const EdgeInsetsDirectional.fromSTEB(14, 10, 14, 10),
+            ),
+            style: TotsTheme()
+                .bodyMedium()
+                .copyWith(
+                  color: TotsTheme().primaryText,
+                  fontSize: 16,
+                ),
+            keyboardType: TextInputType.emailAddress,
+            validator: emailAddressFieldControllerValidator,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSubtitle() {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+      child: SelectionArea(
+          child: Text(widget.subtitle!,
+            style: TotsTheme().bodySmall(),
+      )),
+    );
+  }
+
+  Widget _buildTitle() {
+    return SelectionArea(
+      child: Text(
+      widget.title!,
+      style: TotsTheme().headlineSmall().copyWith(
+          fontSize: 30,
+          fontWeight: FontWeight.w600)
+    ));
+  }
+
+  Widget _buildLogo() {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 24),
+      child: Material(
+        color: Colors.transparent,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Container(
+          width: 48,
+          height: 48,
+          decoration: BoxDecoration(
+            color: TotsTheme().secondaryBackground,
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: Image.asset(widget.imageLogo!).image,
+            ),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 8,
+                color: Color(0x1917171C),
+                offset: Offset(0, 4),
+                spreadRadius: 0,
+              )
+            ],
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -73,148 +207,27 @@ class _LoginStyleOneScreenState extends State<LoginStyleOneScreen> {
             alignment: AlignmentDirectional(0, 0),
             child: Container(
               width: 360,
-              decoration: BoxDecoration(),
+              decoration: const BoxDecoration(),
               child: Align(
-                alignment: AlignmentDirectional(0, 0),
+                alignment: const AlignmentDirectional(0, 0),
                 child: Column(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    widget.imageLogo != null ? _buildLogo() : Container(),
+                    widget.title != null ? _buildTitle() : Container(),
+                    widget.subtitle != null ? _buildSubtitle() : Container(),
                     Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 24),
-                      child: Material(
-                        color: Colors.transparent,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Container(
-                          width: 48,
-                          height: 48,
-                          decoration: BoxDecoration(
-                            color: TotsTheme().secondaryBackground,
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: Image.asset(
-                                'assets/images/DAAsset_1.png',
-                              ).image,
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 8,
-                                color: Color(0x1917171C),
-                                offset: Offset(0, 4),
-                                spreadRadius: 0,
-                              )
-                            ],
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SelectionArea(
-                        child: Text(
-                      'Log in to your account',
-                      style: TotsTheme().headlineSmall().copyWith(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w600)
-                    )),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
-                      child: SelectionArea(
-                          child: Text(
-                        'Welcome back! Please enter your details.',
-                        style: TotsTheme().bodySmall(),
-                      )),
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 32, 0, 0),
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 32, 0, 0),
                       child: Form(
                         key: formKey,
                         autovalidateMode: AutovalidateMode.disabled,
                         child: Column(
                           mainAxisSize: MainAxisSize.max,
                           children: [
-                            Column(
-                              mainAxisSize: MainAxisSize.max,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: [
-                                    SelectionArea(
-                                        child: Text(
-                                      'Email',
-                                      style: TotsTheme().bodyMedium().copyWith(fontWeight: FontWeight.w500),
-                                    )),
-                                  ],
-                                ),
-                                Padding(
-                                  padding: EdgeInsetsDirectional.fromSTEB(
-                                      0, 6, 0, 0),
-                                  child: TextFormField(
-                                    controller: emailAddressFieldController,
-                                    obscureText: false,
-                                    decoration: InputDecoration(
-                                      labelStyle: TotsTheme()
-                                          .bodyMedium()
-                                          .copyWith(
-                                            color: TotsTheme().primaryText,
-                                            fontSize: 16,
-                                          ),
-                                      hintText: 'Enter your email',
-                                      hintStyle: TotsTheme()
-                                          .bodyMedium()
-                                          .copyWith(
-                                            color: TotsTheme().secondaryText,
-                                            fontSize: 16,
-                                          ),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                          color: Color(0xFFD0D5DD),
-                                          width: 1,
-                                        ),
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                          color: Color(0x00000000),
-                                          width: 1,
-                                        ),
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      errorBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                          color: Color(0xFFFDA29B),
-                                          width: 1,
-                                        ),
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      focusedErrorBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                          color: Color(0xFFFDA29B),
-                                          width: 1,
-                                        ),
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      contentPadding:
-                                          EdgeInsetsDirectional.fromSTEB(
-                                              14, 10, 14, 10),
-                                    ),
-                                    style: TotsTheme()
-                                        .bodyMedium()
-                                        .copyWith(
-                                          color: TotsTheme().primaryText,
-                                          fontSize: 16,
-                                        ),
-                                    keyboardType: TextInputType.emailAddress,
-                                    validator: emailAddressFieldControllerValidator,
-                                  ),
-                                ),
-                              ],
-                            ),
+                            _buildEmailField(),
                             Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                              padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
                               child: Column(
                                 mainAxisSize: MainAxisSize.max,
                                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -234,8 +247,7 @@ class _LoginStyleOneScreenState extends State<LoginStyleOneScreen> {
                                     ],
                                   ),
                                   Padding(
-                                    padding: EdgeInsetsDirectional.fromSTEB(
-                                        0, 6, 0, 0),
+                                    padding: const EdgeInsetsDirectional.fromSTEB(0, 6, 0, 0),
                                     child: TextFormField(
                                       controller: passwordFieldController,
                                       obscureText: !passwordFieldVisibility,
@@ -286,9 +298,7 @@ class _LoginStyleOneScreenState extends State<LoginStyleOneScreen> {
                                           borderRadius:
                                               BorderRadius.circular(8),
                                         ),
-                                        contentPadding:
-                                            EdgeInsetsDirectional.fromSTEB(
-                                                14, 10, 14, 10),
+                                        contentPadding: const EdgeInsetsDirectional.fromSTEB(14, 10, 14, 10),
                                         suffixIcon: InkWell(
                                           onTap: () => setState(
                                             () => passwordFieldVisibility = !passwordFieldVisibility,
